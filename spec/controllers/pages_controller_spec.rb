208eb067c49431de60220fd5c24a93da7ed3e70d
kl <kltah1@gmail.com>
2011-10-10 11:19:21 +0800

    Added before(:each) for title page to RSpec test

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,16 +3,22 @@ require 'spec_helper'
 describe PagesController do
   render_views
 
+  before(:each) do
+    #
+    @base_title="Ruby on Rails Tutorial Sample App"
+    #
+  end
+
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
-
+    
     it "should have the right title" do
       get 'home'
       response.should have_selector("title",
-                        :content => "Ruby on Rails Tutorial Sample App | Home")
+                                    :content => @base_title + " | Home")
     end
   end
 
@@ -25,8 +31,7 @@ describe PagesController do
     it "should have the right title" do
       get 'contact'
       response.should have_selector("title",
-                        :content =>
-                          "Ruby on Rails Tutorial Sample App | Contact")
+                                    :content => @base_title + " | Contact")
     end
   end
 
@@ -39,9 +44,8 @@ describe PagesController do
     it "should have the right title" do
       get 'about'
       response.should have_selector("title",
-                        :content =>
-                          "Ruby on Rails Tutorial Sample App | About")
-    end  
+                                    :content => @base_title + " | About")
+    end
   end
   
   describe "GET 'help'" do
@@ -49,14 +53,11 @@ describe PagesController do
       get 'help'
       response.should be_success
     end
-    
-    it "should have the right title" do
-       get 'help'
-       response.should have_selector("title",
-                         :content =>
-                           "Ruby on Rails Tutorial Sample App | Help")
-     end
 
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title",
+                                    :content => @base_title + " | Help")
+    end
   end
-  
 end

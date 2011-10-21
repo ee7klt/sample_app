1348de8019024246a078194d108335ebbb99caef
kl <kltah1@gmail.com>
2011-10-21 11:42:26 +0800

    added precompile line to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,4 +57,7 @@ SampleApp::Application.configure do
 
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
+  
+  # for css
+  config.assets.precompile += %w( *.js *.css )
 end

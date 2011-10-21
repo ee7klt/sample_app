ebc79703e24301fff6360ba121a3f9e393f26711
kl <kltah1@gmail.com>
2011-10-21 11:19:52 +0800

    added mongoid

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SampleApp::Application.initialize!
+
+# in your initializer block
+config.gem "mongo_mapper"
